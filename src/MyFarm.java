f5f56347b976d2d74dc9bdde99e1d5f6f9253627
brianxi <brian_xi25@milton.edu>
2023-05-09 15:14:36 -0400

    Finished

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -13,7 +13,7 @@ public class MyFarm {
 //        totalPlants();
 //        printPlantNames();
 //        printRowInfo();
-//       addLettuce();
+//        addLettuce();
 //        addTulip();
 //        tomatoLocations();
 //        totalCarrots();
@@ -25,8 +25,11 @@ public class MyFarm {
 //        plantWithMaxNumber();
 //        plantWithMinNumber();
 //        plantWithMaxNumberNeedsWater();
-        greatestDifferenceNumberBetweenAnyPlants();
+//        greatestDifferenceNumberBetweenAnyPlants();
 //        numberOfCarrotPlotsNextToLettuce();
+//        removeFirstCarrot();
+//        removeAllEmpty();
+//        greatestDifferenceOfPlantNumbers();
     }
     public void totalPlants() {
         int total = 0;
@@ -37,6 +40,7 @@ public class MyFarm {
         System.out.println("Total Amount of Plants: " + total);
     }
     public void printPlantNames(){
+        System.out.println();
         System.out.println("*******PLANT NAMES*******");
         for(int x=0;x<row.size();x++){
             System.out.print(x + ": " + row.get(x).plantName + " ");
@@ -290,23 +294,119 @@ public class MyFarm {
                     minName = row.get(x).plantName;
             }
         }
-        printRowInfo();
         System.out.println("*******GREATEST DIFFERENCE BETWEEN ANY PLANTS********");
         System.out.println("The greatest difference is between " + maxName + " and " + minName + " with a difference of " + (currentMax-currentMin) + ".");
     }
-//    public void numberOfCarrotPlotsNextToLettuce() {
-//        int plotNumber = 0;
-//        for(int x=0; x<row.size(); x++){
-//            if(row.get(x).plantName.equals("Carrot")){
-//                if(row.get(x+1).plantName.equals("Lettuce")){
-//                    plotNumber = plotNumber +1;
-//                }
-//                if(row.get(x-1).plantName.equals("Lettuce")){
-//                    plotNumber = plotNumber + 1;
-//                }
-//            }
-//        }
-//        System.out.println("********CARROT PLOTS NEXT TO LETTUCE PLOTS********");
-//        System.out.println("There are " + plotNumber + " lettuce plots bordering carrot plots.");
-//    }
+    public void numberOfCarrotPlotsNextToLettuce() {
+        int plotNumber = 0;
+        for(int x=0; x<row.size(); x++){
+            if(row.get(x).plantName.equals("Lettuce")){
+                if(row.get(x+1).plantName.equals("Carrot")){
+                    plotNumber = plotNumber +1;
+                }
+                if(x!=0){
+                    if(row.get(x-1).plantName.equals("Lettuce")){
+                        plotNumber = plotNumber + 1;
+                    }
+                }
+            }
+        }
+        printPlantNames();
+        System.out.println("********CARROT PLOTS NEXT TO LETTUCE PLOTS********");
+        System.out.println("There are " + plotNumber + " lettuce plots bordering carrot plots.");
+    }
+    public void removeFirstCarrot() {
+        printPlantNames();
+        boolean firstCarrot = false;
+        for(int x = 0; x<row.size(); x++){
+            if(row.get(x).plantName.equals("Carrot") && firstCarrot == false){
+                row.remove(x);
+                firstCarrot = true;
+            }
+        }
+        printPlantNames();
+    }
+    public void removeAllEmpty() {
+        printPlantNames();
+        for(int x=0; x<row.size();x++){
+            if(row.get(x).plantName.equals("empty")){
+                row.remove(x);
+                x = x-1;
+            }
+        }
+        printPlantNames();
+    }
+    public void greatestDifferenceOfPlantNumbers() {
+        int maxTomato = 0;
+        int minTomato = 100;
+        int maxLettuce = 0;
+        int minLettuce = 100;
+        int maxTulip = 0;
+        int minTulip = 100;
+        int maxCarrot = 0;
+        int minCarrot = 100;
+        for(int x =0; x<row.size(); x++){
+            if(row.get(x).plantName.equals("Tomato")){
+                if(row.get(x).numberOfPlants > maxTomato){
+                    maxTomato = row.get(x).numberOfPlants;
+                }
+                if(row.get(x).numberOfPlants < minTomato){
+                    minTomato = row.get(x).numberOfPlants;
+                }
+            }
+            if(row.get(x).plantName.equals("Lettuce")){
+                if(row.get(x).numberOfPlants > maxLettuce){
+                    maxLettuce = row.get(x).numberOfPlants;
+                }
+                if(row.get(x).numberOfPlants < minLettuce){
+                    minLettuce = row.get(x).numberOfPlants;
+                }
+            }
+            if(row.get(x).plantName.equals("Tulip")){
+                if(row.get(x).numberOfPlants > maxTulip){
+                    maxTulip = row.get(x).numberOfPlants;
+                }
+                if(row.get(x).numberOfPlants < minTulip){
+                    minTulip = row.get(x).numberOfPlants;
+                }
+            }
+            if(row.get(x).plantName.equals("Carrot")) {
+                if (row.get(x).numberOfPlants > maxCarrot) {
+                    maxCarrot = row.get(x).numberOfPlants;
+                }
+                if (row.get(x).numberOfPlants < minCarrot) {
+                    minCarrot = row.get(x).numberOfPlants;
+                }
+            }
+        }
+        System.out.println("*******GREATEST DIFFERENCES*******");
+        if(maxTomato == 0 || minTomato == 100){
+            System.out.println("There are no tomato plots in this row.");
+        }else if(maxTomato == minTomato){
+            System.out.println("There is only 1 tomato plot.");
+        }else{
+            System.out.println("The greatest difference between different tomato plots is: " + (maxTomato - minTomato) +".");
+        }
+        if(maxLettuce == 0 || minLettuce == 100){
+            System.out.println("There are no lettuce plots in this row.");
+        }else if(maxLettuce == minLettuce){
+            System.out.println("There is only 1 lettuce plot.");
+        }else{
+            System.out.println("The greatest difference between different lettuce plots is: " + (maxLettuce - minLettuce) +".");
+        }
+        if(maxTulip == 0 || minTulip == 100){
+            System.out.println("There are no tulip plots in this row.");
+        }else if(maxTulip == minTulip){
+            System.out.println("There is only 1 tulip plot.");
+        }else{
+            System.out.println("The greatest difference between different tulip plots is: " + (maxTulip - minTulip) +".");
+        }
+        if(maxCarrot == 0 || minCarrot == 100){
+            System.out.println("There are no carrot plots in this row.");
+        }else if(maxCarrot == minCarrot){
+            System.out.println("There is only 1 carrot plot.");
+        }else{
+            System.out.println("The greatest difference between different carrot plots is: " + (maxCarrot - minCarrot) +".");;
+        }
+    }
 }
